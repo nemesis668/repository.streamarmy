6af0f7b3bcc82d403d43ad731946b73aaeba5058
nemesis668 <nemzzybuilds@gmail.com>
2018-01-02 23:25:13 +0000

    fap fix
    fix

--- plugin.video.fapzone/pyxbmct/addonwindow.py
+++ plugin.video.fapzone/pyxbmct/addonwindow.py
@@ -15,7 +15,7 @@ import xbmc
 import xbmcgui
 from xbmcaddon import Addon
 
-_images = os.path.join(Addon('plugin.video.guit').getAddonInfo('path'),'resources', 'kong')
+_images = os.path.join(Addon('plugin.video.fapzone').getAddonInfo('path'),'resources', 'fap')
 
 # Text alighnment constants. Mixed variants are obtained by bit OR (|)
 ALIGN_LEFT = 0
